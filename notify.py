71da79b24d0dd40ca09c0535a3bedac04dae87ff
Arajen Kajanthirabalan <arajenkajanthira@gmail.com>
2026-03-12 20:47:12 -0400

    remove pync and notifier, use plyer for crossplatform functionality

--- notify.py
+++ notify.py
@@ -1,7 +1,7 @@
 from plyer import notification
 from models import Subscription
 from datetime import date
-#from pync import Notifier
+
 def notify(expiring_trials):
     for trial in expiring_trials:
         notification.notify(
@@ -10,9 +10,3 @@ def notify(expiring_trials):
             timeout=10
         )
 
-# def notify(expiring_trials):
-#     for trial in expiring_trials:
-#         Notifier.notify(
-#             f"Your {trial.name} trial expires in {(trial.trial_end_date - date.today()).days} days",
-#             title="Trial Expiring!"
-#         )
